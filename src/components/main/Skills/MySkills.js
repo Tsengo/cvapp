983aeaee95d859aea4e7d3fe13518a143fc36502
Ts3ngo <101639555+Tsengo@users.noreply.github.com>
2023-03-21 11:45:37 +0400

    Update MySkills.js

--- src/components/main/Skills/MySkills.js
+++ src/components/main/Skills/MySkills.js
@@ -8,12 +8,18 @@ const MySkills = () => {
             <ul className='column'>
                 <li><span className='HTML'></span>HTML</li>
                 <li><span className='CSS'></span>CSS</li>
-                <li><span className='PYTHON'></span>PYTHON</li>
+                <li><span className='SCSS'></span>SCSS</li>
+                <li><span className='Boot'></span>Bootstrap</li>
+                <li><span className='JAVASCRIPT'></span>JAVASCRIPT</li>
+                <li><span className='REACT'></span>REACT</li>
             </ul>
             <ul className='column'>
-                <li><span className='REACT'></span>REACT</li>
-                <li><span className='VUE'></span>VUE</li>
-                <li><span className='JAVASCRIPT'></span>JAVASCRIPT</li>
+                <li><span className='nw'></span>NW.js</li>
+                <li><span className='Electron'></span>Electron</li>
+                <li><span className='PYTHON'></span>PYTHON (Basic)</li>
+                <li><span className='Node'></span>Node js (Basic)</li>
+                <li><span className='mysql'></span>MYSQL (Basic)</li>
+                <li><span className='sql'></span>SQL Server (Basic)</li>
             </ul>
         </div>
     )
